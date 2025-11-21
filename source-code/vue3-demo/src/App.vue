822e3c368f25f92c078fef2cfd2a927840a8facf
LBJhui <690306059@qq.com>
2025-11-21 16:32:10 +0800

    2025-11-21

--- source-code/vue3-demo/src/App.vue
+++ source-code/vue3-demo/src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>APP.vue</div>
+  <div>APP.vue {{ count }}</div>
   <el-button type="primary">111</el-button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
 
 <style scoped lang=""></style>
