637474c16e6acb7d3d669892c2a60220d9441a27
LBJhui <690306059@qq.com>
2026-01-27 16:31:49 +0800

    2026-01-27

--- docs/website/language/CSS/CSS单位.md
+++ docs/website/language/CSS/CSS单位.md
@@ -17,3 +17,19 @@
 ## CSS 新增的单位 ch
 
 在 CSS 中，`ch` 是一个相对单位，它代表数字 0（零）的宽度，在当前的字体和字体大小下的度量。这个单位特别适用于需要基于字符宽度进行布局的场景，比如保持文本的垂直对齐或者在元素内部确保一定的空间以容纳文本字符。
+
+%——百分比
+in——寸
+cm——厘米
+mm——毫米
+pt——point，大约 1/72 寸；
+pc——pica，大约 6pt，1/6 寸；
+px——屏幕的一个像素点；
+em——元素的 font-size；
+ex——font-size 的 x-height 值，为小写字母 x 的高度，通常相当于 font-size 的一半。
+
+ch——字符 0(零)的宽度；
+rem——根元素(html 元素)的 font-size；
+vw——viewpoint width，视窗宽度，1vw 等于视窗宽度的 1%；
+vh——viewpoint height，视窗高度，1vh 等于视窗高度的 1%；
+vmin——vw 和 vh 中较小的那个。
---
+++ docs/website/language/CSS/components/3/shadow.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="demo box-shadow">box-shadow</div>
+    <div class="demo drop-shadow">drop-shadow</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.demo {
+  width: 150px;
+  margin: 20px;
+  display: inline-block;
+  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><circle cx="50" cy="50" r="40" fill="deepskyblue"/></svg>') no-repeat center;
+  background-size: contain;
+  padding: 20px;
+}
+.box-shadow {
+  box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.5);
+}
+.drop-shadow {
+  filter: drop-shadow(4px 4px 8px rgba(0, 0, 0, 0.5));
+}
+</style>
